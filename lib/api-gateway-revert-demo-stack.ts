67c5011b34d66382dc17de17461ec66d953aed9f
James Delibas <jdelibas@gmail.com>
2022-02-08 12:04:41 +0400

    chore: initial commit

--- lib/api-gateway-revert-demo-stack.ts
+++ lib/api-gateway-revert-demo-stack.ts
@@ -1,16 +1,46 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import { aws_apigateway as apigateway } from 'aws-cdk-lib';
+import { aws_lambda as lambda } from 'aws-cdk-lib';
+import { aws_logs as logs } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as path from 'path';
+
+if (!process.env.DEPLOY_COUNT) {
+  throw new Error('DEPLOY_COUNT is not set');
+}
 
 export class ApiGatewayRevertDemoStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const fn = new lambda.Function(this, 'fn', {
+      code: lambda.Code.fromAsset(path.resolve(__dirname, '../functions')),
+      handler: 'index.handler',
+      runtime: lambda.Runtime.NODEJS_14_X,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ApiGatewayRevertDemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const logGroup = new logs.LogGroup(this, 'logs');
+
+    const api = new apigateway.LambdaRestApi(this, 'restapi', {
+      restApiName: 'cdk-egress-gateway-demo',
+      handler: fn,
+      proxy: false,
+      description: 'This was created by CDK',
+      deployOptions: {
+        stageName: 'default',
+        tracingEnabled: true,
+        metricsEnabled: true,
+        loggingLevel: apigateway.MethodLoggingLevel.INFO,
+        dataTraceEnabled: false,
+        accessLogDestination: new apigateway.LogGroupLogDestination(logGroup),
+        accessLogFormat: apigateway.AccessLogFormat.custom(
+          `${process.env.DEPLOY_COUNT} ${apigateway.AccessLogField.contextRequestId()} ${apigateway.AccessLogField.contextErrorMessage()} ${apigateway.AccessLogField.contextErrorMessageString()}`
+        ),
+      },
+      retainDeployments: true,
+    }); 
+    
+    const items = api.root.addResource('{proxy+}');
+    items.addMethod('any', new apigateway.HttpIntegration('http://amazonaws.com'));
   }
 }
